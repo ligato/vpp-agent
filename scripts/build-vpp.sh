85a0bb8cac1801e17e8e62008f688fa0892b4c32
Rastislav Szabo <raszabo@cisco.com>
2017-07-24 13:52:09 +0200

    cached build of VPP

--- scripts/build-vpp.sh
+++ scripts/build-vpp.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -e
 
-CACHE_DIR=$HOME/build-cache
+CACHE_DIR=$HOME/local/vpp
 VPP_COMMIT="acd4c63e3c6e70ea3f58527d9bace7c0e38df719"
 
 if [ ! -d "$CACHE_DIR" ]; then
     echo "Building VPP binaries."
-    mkdir $CACHE_DIR
-    cd $CACHE_DIR
 
     # build VPP
     git clone https://gerrit.fd.io/r/vpp 
@@ -15,12 +13,14 @@ if [ ! -d "$CACHE_DIR" ]; then
     git checkout ${VPP_COMMIT} 
     yes | make install-dep     
     make bootstrap     
-    make pkg-deb     
+    make pkg-deb
+
+    # copy deb packages to cache dir
+    cp build-root/*.deb $CACHE_DIR
 else
     echo "Using cached VPP binaries from $CACHE_DIR"
 fi
 
 # install VPP
-cd $CACHE_DIR/vpp/build-root
+cd $HOME/local/vpp
 dpkg -i vpp_*.deb vpp-dev_*.deb vpp-lib_*.deb vpp-plugins_*.deb
-
