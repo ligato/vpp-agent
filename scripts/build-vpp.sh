adea3cddc10a1c1f7d1d5455ce8658e095ce01c4
Milan Lenco <milan.lenco@pantheon.tech>
2017-08-22 13:57:54 +0200

    ODPM-525: Fix build-vpp.sh script.

--- scripts/build-vpp.sh
+++ scripts/build-vpp.sh
@@ -3,21 +3,23 @@ set -e
 
 VPP_CACHE_DIR=$HOME/build-cache/vpp
 VPP_COMMIT="5f22f4ddded8ac41487dab3069ff8d77c3916205"
+WORKDIR="$(pwd)"
 
 if [ ! -d "$VPP_CACHE_DIR" ]; then
     echo "Building VPP binaries."
 
     # build VPP
-    git clone https://gerrit.fd.io/r/vpp 
-    cd vpp 
+    git clone https://gerrit.fd.io/r/vpp /tmp/vpp
+    cd /tmp/vpp
     git checkout ${VPP_COMMIT} 
     yes | make install-dep     
     make bootstrap     
     make pkg-deb
+    cd $(WORKDIR}
 
     # copy deb packages to cache dir
     mkdir $VPP_CACHE_DIR
-    cp build-root/*.deb $VPP_CACHE_DIR
+    cp /tmp/vpp/build-root/*.deb $VPP_CACHE_DIR
 else
     echo "Using cached VPP binaries from $VPP_CACHE_DIR"
 fi
