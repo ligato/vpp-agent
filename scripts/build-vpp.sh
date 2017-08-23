95482cb62f95650bb09a3fa862c7de264cf8742a
Lukas Macko <lmacko@cisco.com>
2017-08-23 12:51:49 +0200

    Download vpp deb packages frrom docker image

--- scripts/build-vpp.sh
+++ scripts/build-vpp.sh
@@ -3,43 +3,44 @@ set -e
 
 VPP_CACHE_DIR=$HOME/build-cache/vpp
 VPP_COMMIT="5f22f4ddded8ac41487dab3069ff8d77c3916205"
+VPP_IMG_TAG="5f22f4d"
 WORKDIR="$(pwd)"
 
 if [ ! -d "$VPP_CACHE_DIR" ]; then
     echo "Building VPP binaries."
 
-    date
-    echo "!! start"
-    # latest vpp requires newer NASM
-    wget http://www.nasm.us/pub/nasm/releasebuilds/2.12.01/nasm-2.12.01.tar.bz2
-    tar xfj nasm-2.12.01.tar.bz2
+    IMG_NAME=ligato/vppdeb:${VPP_IMG_TAG}
+    docker pull ${IMG_NAME}
+    id=$(docker create ${IMG_NAME})
+    docker cp $id:/vpp-deb/vpp.tar .
+    docker rm -v $id
 
-    date
-    echo "!! nasm src cloned"
-    cd nasm-2.12.01/
-    ./autogen.sh
-    ./configure --prefix=/usr/local/
-    make
-    make install
-    cd ..
-    date
-    echo "!! nasm installed"
+    tar -xvf vpp.tar -C $VPP_CACHE_DIR
 
-    # build VPP
-    git clone https://gerrit.fd.io/r/vpp /tmp/vpp
-    cd /tmp/vpp
-    git checkout ${VPP_COMMIT}
-
-    date
-    echo "!! vpp checked out"
-    yes | make install-dep
-    make bootstrap
-    make pkg-deb
-    cd ${WORKDIR}
-
-    # copy deb packages to cache dir
-    mkdir $VPP_CACHE_DIR
-    cp /tmp/vpp/build-root/*.deb $VPP_CACHE_DIR
+#    # latest vpp requires newer NASM
+#    wget http://www.nasm.us/pub/nasm/releasebuilds/2.12.01/nasm-2.12.01.tar.bz2
+#    tar xfj nasm-2.12.01.tar.bz2
+#
+#    cd nasm-2.12.01/
+#    ./autogen.sh
+#    ./configure --prefix=/usr/local/
+#    make
+#    make install
+#    cd ..
+#
+#    # build VPP
+#    git clone https://gerrit.fd.io/r/vpp /tmp/vpp
+#    cd /tmp/vpp
+#    git checkout ${VPP_COMMIT}
+#
+#    yes | make install-dep
+#    make bootstrap
+#    make pkg-deb
+#    cd ${WORKDIR}
+#
+#    # copy deb packages to cache dir
+#    mkdir $VPP_CACHE_DIR
+#    cp /tmp/vpp/build-root/*.deb $VPP_CACHE_DIR
 else
     echo "Using cached VPP binaries from $VPP_CACHE_DIR"
 fi
