4c15259e8521f1364369502c9d0fbf5bad56b7f6
Lukas Macko <lmacko@cisco.com>
2017-09-05 18:34:25 +0200

    Add warn logs

--- vendor/github.com/ligato/cn-infra/messaging/kafka/plugin_impl_kafka.go
+++ vendor/github.com/ligato/cn-infra/messaging/kafka/plugin_impl_kafka.go
@@ -130,11 +130,13 @@ func (p *Plugin) NewAsyncPublisher(topic string, successClb func(messaging.Proto
 
 // NewSyncPublisherToPartition creates a publisher that allows to publish messages to selected topic/partition using synchronous API .
 func (p *Plugin) NewSyncPublisherToPartition(topic string, partition int32) messaging.ProtoPublisher {
+	p.Log.Warn("Publishing to a partition not implemented yet")
 	return p.NewProtoConnection("").NewSyncPublisher(topic)
 }
 
 // NewAsyncPublisherToPartition creates a publisher that allows to publish messages to selected topic/partition using asynchronous API.
 func (p *Plugin) NewAsyncPublisherToPartition(topic string, partition int32, successClb func(messaging.ProtoMessage), errorClb func(messaging.ProtoMessageErr)) messaging.ProtoPublisher {
+	p.Log.Warn("Publishing to a partition not implemented yet")
 	return p.NewProtoConnection("").NewAsyncPublisher(topic, successClb, errorClb)
 }
 
