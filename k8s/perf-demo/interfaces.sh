3c80985ecf582e640df78f22e97fe167e2a0290a
Rastislav Szabo <raszabo@cisco.com>
2017-09-09 22:29:55 +0200

    fixed interfaces script

--- k8s/perf-demo/interfaces.sh
+++ k8s/perf-demo/interfaces.sh
@@ -9,5 +9,5 @@ old_if2=${IF2//\//\\\/}
 new_if1=${1//\//\\\/}
 new_if2=${2//\//\\\/}
 
-sed -i "s/${old_if1}/${new_if1}/g" ./scenario1/etcd.txt
-sed -i "s/${old_if2}/${new_if2}/g" ./scenario1/etcd.txt
+sed -i "s/${old_if1}/${new_if1}/g" ./scenario1/etcd.txt ./scenario2/etcd.txt ./scenario4/etcd.txt
+sed -i "s/${old_if2}/${new_if2}/g" ./scenario1/etcd.txt ./scenario2/etcd.txt ./scenario4/etcd.txt
