410d0d515af1d191ef061ea1c33a9eb809e9514b
Vladimir Lavor <vlavor@cisco.com>
2017-10-11 17:42:52 +0200

    improved stopwatch log/print
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- vendor/github.com/ligato/cn-infra/logging/measure/stopwatch.go
+++ vendor/github.com/ligato/cn-infra/logging/measure/stopwatch.go
@@ -18,7 +18,6 @@ import (
 	"reflect"
 	"github.com/ligato/cn-infra/logging"
 	"time"
-	"strconv"
 	"sync"
 	"fmt"
 )
@@ -29,9 +28,9 @@ type Stopwatch struct {
 	name string
 	// logger used while printing
 	logger logging.Logger
-	// map where measurements are stored
+	// map where measurements are stored. Map is in format [string][]Duration, for every binapi/netlink api there is
+	// a set of times this binapi/netlink was called
 	timeTable sync.Map
-	//timeTable map[string]time.Duration
 	// used to lock map
 	mx sync.Mutex
 }
@@ -59,46 +58,47 @@ func (st *Stopwatch) LogTimeEntry(n interface{}, d time.Duration) {
 	default:
 		name = reflect.TypeOf(n).String()
 	}
-	// index multiple occurrences of the same name (bin_api, link)
-	_, found := st.timeTable.Load(name)
+	// look for entries with the same name
+	v, found := st.timeTable.Load(name)
 	if found {
-		index := 1
-		for {
-			indexed := name + "#" + strconv.Itoa(index)
-			_, found = st.timeTable.Load(indexed)
-			if found {
-				index++
-				continue
-			}
-			name = indexed
-			break
+		durations, ok := v.([]time.Duration)
+		if !ok {
+			panic("cannot cast timeTable map value to duration")
 		}
+		st.timeTable.Store(name, append(durations, d))
+	} else {
+		// Store first time value for the specific key
+		durations := make([]time.Duration, 0)
+		st.timeTable.Store(name, append(durations, d))
 	}
-	// Store time value
-	st.timeTable.Store(name, d)
 }
 
 // Print logs all entries from the map (partial times) + overall time if set
 func (st *Stopwatch) Print() {
 	isMapEmpty := true
 	var wasErr error
+	// Calculate overall time
 	var overall time.Duration
 	st.timeTable.Range(func(k, v interface{}) bool {
+		// Remember that the map contained entries
 		isMapEmpty = false
 		key, ok := k.(string)
 		if !ok {
 			wasErr = fmt.Errorf("cannot cast timeTable map key to string")
-			// false stops the iteration
+			// stops the iteration
 			return false
 		}
-		value, ok := v.(time.Duration)
+		value, ok := v.([]time.Duration)
 		if !ok {
 			wasErr = fmt.Errorf("cannot cast timeTable map value to duration")
-			// false stops the iteration
+			// stops the iteration
 			return false
 		}
-		overall += value
-		st.logger.WithFields(logging.Fields{"conf": st.name, "durationInNs": value.Nanoseconds()}).Infof("%v call took %v", key, value)
+		// Print time value for every and calculate overall time
+		for _, entry := range value {
+			overall += entry
+			st.logger.WithFields(logging.Fields{"conf": st.name, "durationInNs": entry.Nanoseconds()}).Infof("%v call took %v", key, entry)
+		}
 		return true
 	})
 
@@ -107,9 +107,11 @@ func (st *Stopwatch) Print() {
 		panic(wasErr)
 	}
 
+	// In case map is entry
 	if isMapEmpty {
 		st.logger.WithField("conf", st.name).Infof("stopwatch has no entries")
 	}
+	// Log overall time
 	st.logger.WithFields(logging.Fields{"conf": st.name, "durationInNs": overall.Nanoseconds()}).Infof("partial resync time is %v", overall)
 
 	// clear map after use
