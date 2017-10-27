49fb411157bced324ed00f2e9faa5a55d8758906
Vladimir Lavor <vlavor@cisco.com>
2017-10-27 14:13:39 +0200

    implement resync
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- plugins/defaultplugins/l4plugin/data_resync.go
+++ plugins/defaultplugins/l4plugin/data_resync.go
@@ -14,14 +14,46 @@
 
 package l4plugin
 
-import "github.com/ligato/vpp-agent/plugins/defaultplugins/l4plugin/model/l4"
+import (
+	"github.com/ligato/vpp-agent/plugins/defaultplugins/l4plugin/model/l4"
+)
 
-// Resync configures app namespaces to the empty VPP
+// ResyncAppNs configures app namespaces to the empty VPP
 func (plugin *L4Configurator) ResyncAppNs(appNamespaces []*l4.AppNamespaces_AppNamespace) error {
+	plugin.Log.WithField("cfg", plugin).Debug("RESYNC application namespaces begin. ")
+	// Calculate and log application namespaces resync
+	defer func() {
+		if plugin.Stopwatch != nil {
+			plugin.Stopwatch.PrintLog()
+		}
+	}()
+
+	var wasError error
+	if len(appNamespaces) > 0 {
+		for _, appNs := range appNamespaces {
+			wasError = plugin.ConfigureAppNamespace(appNs)
+		}
+	}
+	plugin.Log.WithField("cfg", plugin).Debug("RESYNC application namespaces end. ", wasError)
+
 	return nil
 }
 
-// Resync configures app namespaces to the empty VPP
+// ResyncFeatures sets L4Features flag
 func (plugin *L4Configurator) ResyncFeatures(l4Features *l4.L4Features) error {
+	plugin.Log.WithField("cfg", plugin).Debug("RESYNC L4Features begin. ")
+	// Calculate and log L4Features resync
+	defer func() {
+		if plugin.Stopwatch != nil {
+			plugin.Stopwatch.PrintLog()
+		}
+	}()
+
+	var wasError error
+	if l4Features != nil {
+		wasError = plugin.ConfigureL4FeatureFlag(l4Features)
+	}
+	plugin.Log.WithField("cfg", plugin).Debug("RESYNC L4Features end. ", wasError)
+
 	return nil
 }
