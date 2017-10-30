680f5f910234c19b6fa00fae37e979e1f8aa306d
Ondrej Fabry <ondrej.fabry@pantheon.tech>
2017-10-30 11:15:58 +0100

    Change back to hard coded variable

--- scripts/build-vpp.sh
+++ scripts/build-vpp.sh
@@ -3,7 +3,7 @@ set -e
 
 VPP_CACHE_DIR=$HOME/build-cache/vpp
 
-VPP_COMMIT=`git submodule status vpp | awk '{print $1}'`
+VPP_COMMIT="92b0275a364022af6ab828dfac83e38c0117cfe6"
 VPP_IMG_TAG=`echo ${VPP_COMMIT} | cut -c1-7`
 
 # check if cache folder contains same version
