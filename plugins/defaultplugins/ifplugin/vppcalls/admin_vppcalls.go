a4723902fa16a65b02f85bdd46706517da733466
Ondrej Fabry <ofabry@cisco.com>
2017-12-11 11:05:35 +0100

    Include relevant request info in error message

--- plugins/defaultplugins/ifplugin/vppcalls/admin_vppcalls.go
+++ plugins/defaultplugins/ifplugin/vppcalls/admin_vppcalls.go
@@ -24,7 +24,7 @@ import (
 )
 
 // InterfaceAdminDown calls binary API SwInterfaceSetFlagsReply with AdminUpDown=0.
-func InterfaceAdminDown(ifIdx uint32, vppChan VPPChannel, timeLog *measure.TimeLog) error {
+func InterfaceAdminDown(ifIdx uint32, vppChan VPPChannel, timeLog measure.StopWatchEntry) error {
 	// SwInterfaceSetFlags time measurement
 	start := time.Now()
 	defer func() {
@@ -33,21 +33,7 @@ func InterfaceAdminDown(ifIdx uint32, vppChan VPPChannel, timeLog *measure.TimeL
 		}
 	}()
 
-	// Prepare the message.
-	req := &interfaces.SwInterfaceSetFlags{}
-	req.SwIfIndex = ifIdx
-	req.AdminUpDown = 0
-
-	reply := &interfaces.SwInterfaceSetFlagsReply{}
-	err := vppChan.SendRequest(req).ReceiveReply(reply)
-	if err != nil {
-		return err
-	}
-	if 0 != reply.Retval {
-		return fmt.Errorf("setting of interface flags returned %d", reply.Retval)
-	}
-
-	return nil
+	return interfaceSetFlags(ifIdx, false, vppChan)
 }
 
 // InterfaceAdminUp calls binary API SwInterfaceSetFlagsReply with AdminUpDown=1.
@@ -60,20 +46,28 @@ func InterfaceAdminUp(ifIdx uint32, vppChan VPPChannel, timeLog measure.StopWatc
 		}
 	}()
 
+	return interfaceSetFlags(ifIdx, true, vppChan)
+}
+
+func interfaceSetFlags(ifIdx uint32, adminUp bool, vppChan VPPChannel) error {
 	// Prepare the message.
-	req := &interfaces.SwInterfaceSetFlags{}
-	req.SwIfIndex = ifIdx
-	req.AdminUpDown = 1
+	req := &interfaces.SwInterfaceSetFlags{
+		SwIfIndex: ifIdx,
+	}
+	if adminUp {
+		req.AdminUpDown = 1
+	} else {
+		req.AdminUpDown = 0
+	}
 
 	reply := &interfaces.SwInterfaceSetFlagsReply{}
 	err := vppChan.SendRequest(req).ReceiveReply(reply)
 	if err != nil {
 		return err
 	}
-	if 0 != reply.Retval {
-		return fmt.Errorf("setting of interface flags returned %d", reply.Retval)
+	if reply.Retval != 0 {
+		return fmt.Errorf("setting of interface flags (%+v) returned %d", req, reply.Retval)
 	}
 
 	return nil
-
 }
