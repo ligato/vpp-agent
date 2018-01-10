dfa2134a3d94d546d4c7c7ad015120ac18422d73
Ondrej Fabry <ofabry@cisco.com>
2018-01-10 11:28:53 +0100

    Upgrade VPP to v18.04-rc0+33 (b59bd65)

--- scripts/build-vpp.sh
+++ scripts/build-vpp.sh
@@ -3,7 +3,7 @@ set -e
 
 VPP_CACHE_DIR=$HOME/build-cache/vpp
 
-VPP_COMMIT="2877eee189993dbd1b9a5c3d22499930a4768786"
+VPP_COMMIT="b59bd659388db62b60a4fb887ce272989d7c340c"
 VPP_IMG_TAG=`echo ${VPP_COMMIT} | cut -c1-7`
 
 # check if cache folder contains same version
