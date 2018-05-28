44d485c18f6f7beea89d84053418363b01bac7fa
Ondrej Fabry <ofabry@cisco.com>
2018-05-28 20:47:08 +0200

    Introduce process for verifying VPP binapi compatibility
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- scripts/genjsonapi.sh
+++ scripts/genjsonapi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 VPPDIR=${VPPDIR:-vpp}
+APIDIR=${APIDIR:-/usr/share/vpp/api}
 
-find ${VPPDIR} -name \*.api -printf 'echo %p && ${VPPDIR}/src/tools/vppapigen/vppapigen --includedir ${VPPDIR}/src --input %p --output /usr/share/vpp/api/%f.json JSON\n' | xargs -0 sh -c
+mkdir -p ${APIDIR}
+
+find ${VPPDIR} -name \*.api -printf "echo %p - ${APIDIR}/%f.json && ${VPPDIR}/src/tools/vppapigen/vppapigen --includedir ${VPPDIR}/src --input %p --output ${APIDIR}/%f.json JSON\n" | xargs -0 sh -c
