64c76153e6f997214d98ba6bfd4eb66e192f6a05
Ondrej Fabry <ofabry@cisco.com>
2018-06-01 09:42:49 +0200

    Edit message phrasing
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- docker/prod/exec_agent.sh
+++ docker/prod/exec_agent.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -n "$OMIT_AGENT" ]; then
-    echo "Start of vpp-agent is omitted (unset OMIT_AGENT to disable it)"
+    echo "Start of vpp-agent disabled (unset OMIT_AGENT to enable it)"
 else
     echo "Starting vpp-agent.."
     exec vpp-agent --config-dir=/opt/vpp-agent/dev
