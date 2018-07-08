60014bfdf657705539a4776687f6866b048f01f5
Marcel Sestak <msestak@cisco.com>
2018-07-08 21:33:51 +0200

    fixes to ipv6 tests
    Signed-off-by: Marcel Sestak <msestak@cisco.com>

--- tests/robot/libraries/linux.py
+++ tests/robot/libraries/linux.py
@@ -44,41 +44,17 @@ def Check_Linux_Interface_IP_Presence(data, mac, ip):
     present_mac = False
     present_ip = False
     for iface in data:
-        if data[iface]["mac"] == mac:
-            present_mac = True
-        if data[iface]["ipv4"] == ip:
-            present_ip = True
+        if  "mac" in  data[iface]:
+           if data[iface]["mac"] == mac:
+              present_mac = True
+        if "ipv4" in data[iface]:
+           if data[iface]["ipv4"] == ip:
+              present_ip = True
+        if "ipv6" in data[iface]:
+           if data[iface]["ipv6"] == ip:
+              present_ip = True
     if present_mac == True and present_ip == True:
         return True
     else:
         return False
 
-x='''1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1
-    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-    inet 127.0.0.1/8 scope host lo
-       valid_lft forever preferred_lft forever
-    inet6 ::1/128 scope host 
-       valid_lft forever preferred_lft forever
-2: vpp1_veth2@vpp1_veth1: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default 
-    link/ether a2:a1:a1:a1:a1:a1 brd ff:ff:ff:ff:ff:ff
-    inet6 fe80::14f3:b3ff:fe13:a7cb/64 scope link 
-       valid_lft forever preferred_lft forever
-3: vpp1_veth1@vpp1_veth2: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default 
-    link/ether 12:11:11:11:11:11 brd ff:ff:ff:ff:ff:ff
-    inet 10.10.1.1/24 scope global vpp1_veth1
-       valid_lft forever preferred_lft forever
-    inet6 fe80::1011:11ff:fe11:1111/64 scope link 
-       valid_lft forever preferred_lft forever
-4: linux_vpp1_tap1: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UNKNOWN group default qlen 1000
-    link/ether e6:45:54:36:94:2d brd ff:ff:ff:ff:ff:ff
-    inet6 fe80::e445:54ff:fe36:942d/64 scope link 
-       valid_lft forever preferred_lft forever
-2962: eth0@if2963: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default 
-    link/ether 02:42:ac:11:00:05 brd ff:ff:ff:ff:ff:ff link-netnsid 0
-    inet 172.17.0.5/16 scope global eth0
-       valid_lft forever preferred_lft forever
-    inet6 fe80::42:acff:fe11:5/64 scope link 
-       valid_lft forever preferred_lft forever'''
-
-#Parse_Linux_Interfaces(x)
-#Pick_Linux_Interface(x,"vpp1_veth1@vpp1_veth2")
