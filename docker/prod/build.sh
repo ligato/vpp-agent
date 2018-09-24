3d679a978f605487f8794c176d6f58aaa2d1a473
Stanislav Chlebec <stanislav.chlebec@pantheon.tech>
2018-09-24 12:45:03 +0000

    Add arm64 support for contiv/vpp docker images building         The naming rules of the built docker images is given below:         1. For x86_64 arch, it would be named as: dev-vpp-agent-amd64:latest         2. For arm64 arch, it would be named as: dev-vpp-agent-arm64:latest         3. The docker images built for amd64 are tagged as the default:         dev-vpp-agent-amd64:latest ---> dev-vpp-agent:latest
            The purpose of doing it in this way is to prepare the enablement
            of the multi-arch support with fat manifest in a certain template
            like:  --template -ARCH: in the future.
    
            Previous behaviour of this script was preserved not to disrupt current ways of use
            (pre-set environmental variable will be taken into account...).
    
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- docker/prod/build.sh
+++ docker/prod/build.sh
@@ -4,6 +4,35 @@ cd "$(dirname "$0")"
 
 set -e
 
-IMAGE_TAG=${IMAGE_TAG:-'prod_vpp_agent'}
+#To prepare for future fat manifest image by multi-arch manifest,
+#now build the docker image with its arch
+#For fat manifest, please refer
+#https://docs.docker.com/registry/spec/manifest-v2-2/#example-manifest-list
+
+BUILDARCH=`uname -m`
+
+if [ ${BUILDARCH} = "aarch64" ] ; then
+  IMAGE_TAG=${IMAGE_TAG:-'prod_vpp_agent-arm64'}
+  # Dockerfile  for prod_vpp_agent expects that dev-vpp-agent:latest image is built
+  sudo docker tag dev_vpp_agent-arm64:latest dev_vpp_agent:latest
+fi
+
+
+if [ ${BUILDARCH} = "x86_64" ] ; then
+  # for AMD64 platform is used the default image (without suffix -amd64)
+  IMAGE_TAG=${IMAGE_TAG:-'prod_vpp_agent'}
+  # Dockerfile expects that dev-vpp-agent:latest image is built
+  # Here it is granted
+fi
 
 sudo docker build  ${DOCKER_BUILD_ARGS} --tag ${IMAGE_TAG} .
+
+if [[ ${BUILDARCH} = "x86_64" && ${IMAGE_TAG} = "prod_vpp_agent" ]] ; then
+  # tag also explicit docker image on AMD64 platform
+  docker tag  ${IMAGE_TAG}:latest prod_vpp_agent-amd64:latest
+fi
+
+# cleaning
+if [ ${BUILDARCH} = "aarch64" ] ; then
+  sudo docker rmi dev_vpp_agent:latest
+fi
