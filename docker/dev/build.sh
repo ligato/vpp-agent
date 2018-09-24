9c0aa8e645a4c4b9de6d87900a42a1c5e6ae95c1
Stanislav Chlebec <stanislav.chlebec@pantheon.tech>
2018-09-24 11:33:51 +0000

    Add arm64 support for contiv/vpp docker images building     The naming rules of the built docker images is given below:     1. For x86_64 arch, it would be named as: dev-vpp-agent-amd64:latest     2. For arm64 arch, it would be named as: dev-vpp-agent-arm64:latest     3. The docker images built for amd64 are tagged as the default:     dev-vpp-agent-amd64:latest ---> dev-vpp-agent:latest
        The purpose of doing it in this way is to prepare the enablement
        of the multi-arch support with fat manifest in a certain template
        like:  --template -ARCH: in the future.
    
        Previous behaviour of this script was preserved not to disrupt current ways of use
        (pre-set environmental variable will be taken into account...).
    
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- docker/dev/build.sh
+++ docker/dev/build.sh
@@ -4,11 +4,31 @@ cd "$(dirname "$0")"
 
 set -e
 
-IMAGE_TAG=${IMAGE_TAG:-'dev_vpp_agent'}
+#IMAGE_TAG=${IMAGE_TAG:-'dev_vpp_agent'}
 DOCKERFILE=${DOCKERFILE:-'Dockerfile'}
 
 BASE_IMG=${BASE_IMG:-'ubuntu:18.04'}
-GOLANG_OS_ARCH=${GOLANG_OS_ARCH:-'linux-amd64'}
+#GOLANG_OS_ARCH=${GOLANG_OS_ARCH:-'linux-amd64'}
+
+#To prepare for future fat manifest image by multi-arch manifest,
+#now build the docker image with its arch
+#For fat manifest, please refer
+#https://docs.docker.com/registry/spec/manifest-v2-2/#example-manifest-list
+
+BUILDARCH=`uname -m`
+
+if [ ${BUILDARCH} = "aarch64" ] ; then
+  IMAGE_TAG=${IMAGE_TAG:-'dev_vpp_agent-arm64'}
+  GOLANG_OS_ARCH=${GOLANG_OS_ARCH:-'linux-arm64'}
+fi
+
+
+if [ ${BUILDARCH} = "x86_64" ] ; then
+  # for AMD64 platform is used the default image (without suffix -amd64)
+  IMAGE_TAG=${IMAGE_TAG:-'dev_vpp_agent'}
+  GOLANG_OS_ARCH=${GOLANG_OS_ARCH:-'linux-amd64'}
+fi
+
 
 source ../../vpp.env
 VPP_DEBUG_DEB=${VPP_DEBUG_DEB:-}
@@ -17,6 +37,8 @@ VERSION=$(git describe --always --tags --dirty)
 COMMIT=$(git rev-parse HEAD)
 
 echo "=============================="
+echo "Architecture: ${BUILDARCH}"
+echo
 echo "VPP repo URL: ${VPP_REPO_URL}"
 echo "VPP commit:   ${VPP_COMMIT}"
 echo
@@ -37,3 +59,8 @@ docker build -f ${DOCKERFILE} \
     --build-arg VERSION=${VERSION} \
     --build-arg COMMIT=${COMMIT} \
     ${DOCKER_BUILD_ARGS} ../..
+
+if [[ ${BUILDARCH} = "x86_64" && ${IMAGE_TAG} = "dev_vpp_agent" ]] ; then
+  # tag also explicit docker image on AMD64 platform
+  docker tag  ${IMAGE_TAG}:latest dev_vpp_agent-amd64:latest
+fi
