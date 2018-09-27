ed9c6715773a1df766457d891af80235afe14cdf
Stanislav Chlebec <stanislav.chlebec@pantheon.tech>
2018-09-27 06:16:51 +0000

    Fixed small mistake in commnent. Replacement of DEFAULT_IMAGE_NAME by IMAGE_NAME which is shorter.
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- docker/dev/push_image.sh
+++ docker/dev/push_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Usage: examples
 #    ./push_image.sh
-#    BRANCH_HEAD_TAG='git describe' ./push_image.sh
+#    BRANCH_HEAD_TAG=`git describe` ./push_image.sh
 #    REPO_OWNER=stanislavchlebec BRANCH_HEAD_TAG=`git describe` ./push_image.sh
-#    LOCAL_IMAGE='prod_vpp_agent:latest' DEFAULT_IMAGE_NAME='vpp-agent' ./push_image.sh
+#    LOCAL_IMAGE='prod_vpp_agent:latest' IMAGE_NAME='vpp-agent' ./push_image.sh
 
 # Warning: use only IMMEDIATELY after docker/dev/build.sh to prevent INCONSISTENCIES such as 
 #          a) after building image you switch to other branch which will result in mismatch of version of image and its tag
@@ -19,7 +19,7 @@ VERSION=$(git describe --always --tags --dirty)
 
 LOCAL_IMAGE=${LOCAL_IMAGE:-'dev_vpp_agent:latest'}
 REPO_OWNER=${REPO_OWNER:-'ligato'}
-DEFAULT_IMAGE_NAME=${DEFAULT_IMAGE_NAME:-'dev-vpp-agent'}
+IMAGE_NAME=${IMAGE_NAME:-'dev-vpp-agent'}
 
 #To prepare for future fat manifest image by multi-arch manifest,
 #now build the docker image with its arch
@@ -53,26 +53,26 @@ case "${BRANCH_NAME}" in
     if [ ${BRANCH_HEAD_TAG} != "undefined" ] ; then
       if [ ${BUILDARCH} = "x86_64" ] ; then
         # for AMD64 platform is used also the default image (without suffix -amd64)
-        docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}:${BRANCH_HEAD_TAG}
-        docker push ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}:${BRANCH_HEAD_TAG}
-        docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}:latest
-        docker push ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}:latest
+        docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${IMAGE_NAME}:${BRANCH_HEAD_TAG}
+        docker push ${REPO_OWNER}/${IMAGE_NAME}:${BRANCH_HEAD_TAG}
+        docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${IMAGE_NAME}:latest
+        docker push ${REPO_OWNER}/${IMAGE_NAME}:latest
       fi
-      docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_HEAD_TAG}
-      docker push ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_HEAD_TAG}
-      docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:latest
-      docker push ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:latest
+      docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_HEAD_TAG}
+      docker push ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_HEAD_TAG}
+      docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:latest
+      docker push ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:latest
     else
       echo "For branch ${BRANCH_NAME} is no setup for tagging and pushing docker images because HEAD has no tag."
     fi
     ;;
   "(unnamed branch)" )
-    docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:${VERSION}
+    docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:${VERSION}
     echo "Repository is in detached HEAD state - please push manually:"
-    echo docker push ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:${VERSION}
+    echo docker push ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:${VERSION}
     ;;
   * )
-    docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_NAME}
-    docker push ${REPO_OWNER}/${DEFAULT_IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_NAME}
+    docker tag ${LOCAL_IMAGE} ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_NAME}
+    docker push ${REPO_OWNER}/${IMAGE_NAME}-${ARCH_SUFFIX}:${BRANCH_NAME}
     ;;
 esac
