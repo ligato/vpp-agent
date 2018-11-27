46fcf3322d47169f0c75af2316c82b2fdc47e5e8
Vladimir Lavor <vlavor@cisco.com>
2018-11-27 11:00:48 +0100

    update VPP to v19.01-rc0~234-gb11175d7 (vpp checkpoint)
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- plugins/vpp/binapi/ipsec/ipsec.ba.go
+++ plugins/vpp/binapi/ipsec/ipsec.ba.go
@@ -5,8 +5,8 @@
  Package ipsec is a generated from VPP binary API module 'ipsec'.
 
  It contains following objects:
-	 50 messages
-	 25 services
+	 54 messages
+	 27 services
 
 */
 package ipsec
@@ -2476,9 +2476,165 @@ func (*IpsecTunnelIfSetSaReply) GetMessageType() api.MessageType {
 	return api.ReplyMessage
 }
 
+// IpsecBackendDump represents the VPP binary API message 'ipsec_backend_dump'.
+//
+//            "ipsec_backend_dump",
+//            [
+//                "u16",
+//                "_vl_msg_id"
+//            ],
+//            [
+//                "u32",
+//                "client_index"
+//            ],
+//            [
+//                "u32",
+//                "context"
+//            ],
+//            {
+//                "crc": "0x51077d14"
+//            }
+//
+type IpsecBackendDump struct{}
+
+func (*IpsecBackendDump) GetMessageName() string {
+	return "ipsec_backend_dump"
+}
+func (*IpsecBackendDump) GetCrcString() string {
+	return "51077d14"
+}
+func (*IpsecBackendDump) GetMessageType() api.MessageType {
+	return api.RequestMessage
+}
+
+// IpsecBackendDetails represents the VPP binary API message 'ipsec_backend_details'.
+//
+//            "ipsec_backend_details",
+//            [
+//                "u16",
+//                "_vl_msg_id"
+//            ],
+//            [
+//                "u32",
+//                "context"
+//            ],
+//            [
+//                "u8",
+//                "name",
+//                128
+//            ],
+//            [
+//                "u8",
+//                "protocol"
+//            ],
+//            [
+//                "u8",
+//                "index"
+//            ],
+//            [
+//                "u8",
+//                "active"
+//            ],
+//            {
+//                "crc": "0x3ba4d642"
+//            }
+//
+type IpsecBackendDetails struct {
+	Name     []byte `struc:"[128]byte"`
+	Protocol uint8
+	Index    uint8
+	Active   uint8
+}
+
+func (*IpsecBackendDetails) GetMessageName() string {
+	return "ipsec_backend_details"
+}
+func (*IpsecBackendDetails) GetCrcString() string {
+	return "3ba4d642"
+}
+func (*IpsecBackendDetails) GetMessageType() api.MessageType {
+	return api.ReplyMessage
+}
+
+// IpsecSelectBackend represents the VPP binary API message 'ipsec_select_backend'.
+//
+//            "ipsec_select_backend",
+//            [
+//                "u16",
+//                "_vl_msg_id"
+//            ],
+//            [
+//                "u32",
+//                "client_index"
+//            ],
+//            [
+//                "u32",
+//                "context"
+//            ],
+//            [
+//                "u8",
+//                "protocol"
+//            ],
+//            [
+//                "u8",
+//                "index"
+//            ],
+//            {
+//                "crc": "0xf2cc2b47"
+//            }
+//
+type IpsecSelectBackend struct {
+	Protocol uint8
+	Index    uint8
+}
+
+func (*IpsecSelectBackend) GetMessageName() string {
+	return "ipsec_select_backend"
+}
+func (*IpsecSelectBackend) GetCrcString() string {
+	return "f2cc2b47"
+}
+func (*IpsecSelectBackend) GetMessageType() api.MessageType {
+	return api.RequestMessage
+}
+
+// IpsecSelectBackendReply represents the VPP binary API message 'ipsec_select_backend_reply'.
+//
+//            "ipsec_select_backend_reply",
+//            [
+//                "u16",
+//                "_vl_msg_id"
+//            ],
+//            [
+//                "u32",
+//                "context"
+//            ],
+//            [
+//                "i32",
+//                "retval"
+//            ],
+//            {
+//                "crc": "0xe8d4e804"
+//            }
+//
+type IpsecSelectBackendReply struct {
+	Retval int32
+}
+
+func (*IpsecSelectBackendReply) GetMessageName() string {
+	return "ipsec_select_backend_reply"
+}
+func (*IpsecSelectBackendReply) GetCrcString() string {
+	return "e8d4e804"
+}
+func (*IpsecSelectBackendReply) GetMessageType() api.MessageType {
+	return api.ReplyMessage
+}
+
 /* Services */
 
 type Services interface {
+	DumpIpsecBackend(*IpsecBackendDump) (*IpsecBackendDetails, error)
 	DumpIpsecSa(*IpsecSaDump) (*IpsecSaDetails, error)
 	DumpIpsecSpd(*IpsecSpdDump) (*IpsecSpdDetails, error)
 	DumpIpsecSpdInterface(*IpsecSpdInterfaceDump) (*IpsecSpdInterfaceDetails, error)
@@ -2499,6 +2655,7 @@ type Services interface {
 	IpsecInterfaceAddDelSpd(*IpsecInterfaceAddDelSpd) (*IpsecInterfaceAddDelSpdReply, error)
 	IpsecSaSetKey(*IpsecSaSetKey) (*IpsecSaSetKeyReply, error)
 	IpsecSadAddDelEntry(*IpsecSadAddDelEntry) (*IpsecSadAddDelEntryReply, error)
+	IpsecSelectBackend(*IpsecSelectBackend) (*IpsecSelectBackendReply, error)
 	IpsecSpdAddDel(*IpsecSpdAddDel) (*IpsecSpdAddDelReply, error)
 	IpsecSpdAddDelEntry(*IpsecSpdAddDelEntry) (*IpsecSpdAddDelEntryReply, error)
 	IpsecTunnelIfAddDel(*IpsecTunnelIfAddDel) (*IpsecTunnelIfAddDelReply, error)
@@ -2557,4 +2714,8 @@ func init() {
 	api.RegisterMessage((*IpsecTunnelIfSetKeyReply)(nil), "ipsec.IpsecTunnelIfSetKeyReply")
 	api.RegisterMessage((*IpsecTunnelIfSetSa)(nil), "ipsec.IpsecTunnelIfSetSa")
 	api.RegisterMessage((*IpsecTunnelIfSetSaReply)(nil), "ipsec.IpsecTunnelIfSetSaReply")
+	api.RegisterMessage((*IpsecBackendDump)(nil), "ipsec.IpsecBackendDump")
+	api.RegisterMessage((*IpsecBackendDetails)(nil), "ipsec.IpsecBackendDetails")
+	api.RegisterMessage((*IpsecSelectBackend)(nil), "ipsec.IpsecSelectBackend")
+	api.RegisterMessage((*IpsecSelectBackendReply)(nil), "ipsec.IpsecSelectBackendReply")
 }
