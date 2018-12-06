7057f29439ffc54bb4dcdaaa6b3a71e440172523
Ondrej Fabry <ofabry@cisco.com>
2018-12-06 10:40:31 +0100

    Update cn-infra
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- vendor/github.com/ligato/cn-infra/process/options.go
+++ vendor/github.com/ligato/cn-infra/process/options.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package processmanager
+package process
 
 // DefaultPlugin is a default instance of Plugin.
 var DefaultPlugin = *NewPlugin()
