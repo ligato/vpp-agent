53605bc1ea2bca1d08db901fcf150571fec80e1f
Vladimir Lavor <vlavor@cisco.com>
2019-04-10 11:10:27 +0200

    added abfplugin
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

---
+++ plugins/vpp/abfplugin/vppcalls/vpp1901/abf_vppcalls.go
@@ -0,0 +1,170 @@
+//  Copyright (c) 2019 Cisco and/or its affiliates.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package vpp1901
+
+import (
+	"fmt"
+	"net"
+
+	vpp_abf "github.com/ligato/vpp-agent/api/models/vpp/abf"
+
+	"github.com/go-errors/errors"
+	"github.com/ligato/vpp-agent/plugins/vpp/binapi/vpp1901/abf"
+)
+
+// GetAbfVersion retrieves version of the VPP ABF plugin
+func (h *ABFVppHandler) GetAbfVersion() (ver string, err error) {
+	req := &abf.AbfPluginGetVersion{}
+	reply := &abf.AbfPluginGetVersionReply{}
+
+	if err := h.callsChannel.SendRequest(req).ReceiveReply(reply); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%d.%d", reply.Major, reply.Minor), nil
+}
+
+// AddAbfPolicy creates new ABF entry together with a list of forwarding paths
+func (h *ABFVppHandler) AddAbfPolicy(policyID, aclID uint32, abfPaths []*vpp_abf.ABF_ForwardingPath) error {
+	if err := h.abfAddDelPolicy(policyID, aclID, abfPaths, true); err != nil {
+		return errors.Errorf("failed to add ABF policy %d (ACL: %v): %v", policyID, aclID, err)
+	}
+	return nil
+}
+
+// DeleteAbfPolicy removes existing ABF entry
+func (h *ABFVppHandler) DeleteAbfPolicy(policyID, aclID uint32, abfPaths []*vpp_abf.ABF_ForwardingPath) error {
+	if err := h.abfAddDelPolicy(policyID, aclID, abfPaths, false); err != nil {
+		return errors.Errorf("failed to delete ABF policy %d (ACL: %v): %v", policyID, aclID, err)
+	}
+	return nil
+}
+
+// AbfAttachInterfaceIPv4 attaches IPv4 interface to the ABF
+func (h *ABFVppHandler) AbfAttachInterfaceIPv4(policyID, ifIdx, priority uint32) error {
+	if err := h.abfAttachDetachInterface(policyID, ifIdx, priority, true, false); err != nil {
+		return errors.Errorf("failed to attach IPv4 interface %d to ABF policy %d: %v", ifIdx, policyID, err)
+	}
+	return nil
+}
+
+// AbfDetachInterfaceIPv4 detaches IPV4 interface from the ABF
+func (h *ABFVppHandler) AbfDetachInterfaceIPv4(policyID, ifIdx, priority uint32) error {
+	if err := h.abfAttachDetachInterface(policyID, ifIdx, priority, false, false); err != nil {
+		return errors.Errorf("failed to detach IPv4 interface %d from ABF policy %d: %v", ifIdx, policyID, err)
+	}
+	return nil
+}
+
+// AbfAttachInterfaceIPv6 attaches IPv6 interface to the ABF
+func (h *ABFVppHandler) AbfAttachInterfaceIPv6(policyID, ifIdx, priority uint32) error {
+	if err := h.abfAttachDetachInterface(policyID, ifIdx, priority, true, true); err != nil {
+		return errors.Errorf("failed to attach IPv6 interface %d to ABF policy %d: %v", ifIdx, policyID, err)
+	}
+	return nil
+}
+
+// AbfDetachInterfaceIPv6 detaches IPv6 interface from the ABF
+func (h *ABFVppHandler) AbfDetachInterfaceIPv6(policyID, ifIdx, priority uint32) error {
+	if err := h.abfAttachDetachInterface(policyID, ifIdx, priority, false, true); err != nil {
+		return errors.Errorf("failed to detach IPv6 interface %d from ABF policy %d: %v", ifIdx, policyID, err)
+	}
+	return nil
+}
+
+func (h *ABFVppHandler) abfAttachDetachInterface(policyID, ifIdx, priority uint32, isAdd, isIPv6 bool) error {
+	req := &abf.AbfItfAttachAddDel{
+		IsAdd: boolToUint(isAdd),
+		Attach: abf.AbfItfAttach{
+			PolicyID:  policyID,
+			SwIfIndex: ifIdx,
+			Priority:  priority,
+			IsIPv6:    boolToUint(isIPv6),
+		},
+	}
+	reply := &abf.AbfItfAttachAddDelReply{}
+
+	return h.callsChannel.SendRequest(req).ReceiveReply(reply)
+}
+
+func (h *ABFVppHandler) abfAddDelPolicy(policyID, aclID uint32, abfPaths []*vpp_abf.ABF_ForwardingPath, isAdd bool) error {
+	req := &abf.AbfPolicyAddDel{
+		IsAdd: boolToUint(isAdd),
+		Policy: abf.AbfPolicy{
+			PolicyID: policyID,
+			ACLIndex: aclID,
+			Paths:    h.toFibPaths(abfPaths),
+		},
+	}
+	reply := &abf.AbfPolicyAddDelReply{}
+
+	return h.callsChannel.SendRequest(req).ReceiveReply(reply)
+}
+
+func (h *ABFVppHandler) toFibPaths(abfPaths []*vpp_abf.ABF_ForwardingPath) (fibPaths []abf.FibPath) {
+	for _, abfPath := range abfPaths {
+		// label stack
+		var fibPathLabelStack []abf.FibMplsLabel
+		for _, abfPathLabel := range abfPath.LabelStack {
+			fibPathLabel := abf.FibMplsLabel{
+				IsUniform: boolToUint(abfPathLabel.IsUniform),
+				Label:     abfPathLabel.Label,
+				Exp:       uint8(abfPathLabel.Exp),
+				TTL:       uint8(abfPathLabel.TTL),
+			}
+			fibPathLabelStack = append(fibPathLabelStack, fibPathLabel)
+		}
+
+		// fib path
+		ifData, exists := h.ifIndexes.LookupByName(abfPath.InterfaceName)
+		if !exists {
+			continue
+		}
+
+		fibPath := abf.FibPath{
+			SwIfIndex:         ifData.SwIfIndex,
+			TableID:           abfPath.Vrf,
+			Weight:            uint8(abfPath.Weight),
+			Preference:        uint8(abfPath.Preference),
+			IsLocal:           boolToUint(abfPath.Local),
+			IsDrop:            boolToUint(abfPath.Drop),
+			IsUDPEncap:        boolToUint(abfPath.UdpEncap),
+			IsUnreach:         boolToUint(abfPath.Unreachable),
+			IsProhibit:        boolToUint(abfPath.Prohibit),
+			IsResolveHost:     boolToUint(abfPath.ResolveHost),
+			IsResolveAttached: boolToUint(abfPath.ResolveAttached),
+			IsDvr:             boolToUint(abfPath.Dvr),
+			IsSourceLookup:    boolToUint(abfPath.SourceLookup),
+			Afi:               uint8(abfPath.Afi),
+			NextHop:           net.ParseIP(abfPath.NextHopIp),
+			NextHopID:         abfPath.NextHopId,
+			RpfID:             abfPath.RpfId,
+			ViaLabel:          abfPath.ViaLabel,
+			NLabels:           uint8(len(fibPathLabelStack)),
+			LabelStack:        fibPathLabelStack,
+		}
+
+		fibPaths = append(fibPaths, fibPath)
+	}
+
+	return fibPaths
+}
+
+func boolToUint(input bool) uint8 {
+	if input {
+		return 1
+	}
+	return 0
+}
---
+++ plugins/vpp/abfplugin/vppcalls/vpp1901/dump_abf_vppcalls.go
@@ -0,0 +1,187 @@
+//  Copyright (c) 2019 Cisco and/or its affiliates.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package vpp1901
+
+import (
+	"net"
+	"strconv"
+
+	vpp_abf "github.com/ligato/vpp-agent/api/models/vpp/abf"
+	"github.com/ligato/vpp-agent/plugins/vpp/abfplugin/vppcalls"
+	"github.com/ligato/vpp-agent/plugins/vpp/binapi/vpp1901/abf"
+)
+
+// placeholder for unknown names
+const unknownName = "<unknown>"
+
+// DumpABFPolicy retrieves VPP ABF configuration.
+func (h *ABFVppHandler) DumpABFPolicy() ([]*vppcalls.ABFDetails, error) {
+	// retrieve ABF interfaces
+	attachedIfs, err := h.dumpABFInterfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	// retrieve ABF policy
+	abfPolicy, err := h.dumpABFPolicy()
+	if err != nil {
+		return nil, err
+	}
+
+	// merge attached interfaces data to policy
+	for _, policy := range abfPolicy {
+		ifData, ok := attachedIfs[policy.Meta.PolicyID]
+		if ok {
+			policy.ABF.AttachedInterfaces = ifData
+		}
+	}
+
+	return abfPolicy, nil
+}
+
+func (h *ABFVppHandler) dumpABFInterfaces() (map[uint32][]*vpp_abf.ABF_AttachedInterface, error) {
+	// ABF index <-> attached interfaces
+	abfIfs := make(map[uint32][]*vpp_abf.ABF_AttachedInterface)
+
+	req := &abf.AbfItfAttachDump{}
+	reqCtx := h.dumpChannel.SendMultiRequest(req)
+
+	for {
+		reply := &abf.AbfItfAttachDetails{}
+		last, err := reqCtx.ReceiveReply(reply)
+		if err != nil {
+			return nil, err
+		}
+		if last {
+			break
+		}
+
+		// interface name
+		ifName, _, exists := h.ifIndexes.LookupBySwIfIndex(reply.Attach.SwIfIndex)
+		if !exists {
+			ifName = unknownName
+		}
+
+		// attached interface entry
+		attached := &vpp_abf.ABF_AttachedInterface{
+			InputInterface: ifName,
+			Priority:       reply.Attach.Priority,
+			IsIpv6:         uintToBool(reply.Attach.IsIPv6),
+		}
+
+		_, ok := abfIfs[reply.Attach.PolicyID]
+		if !ok {
+			abfIfs[reply.Attach.PolicyID] = []*vpp_abf.ABF_AttachedInterface{}
+		}
+		abfIfs[reply.Attach.PolicyID] = append(abfIfs[reply.Attach.PolicyID], attached)
+	}
+
+	return abfIfs, nil
+}
+
+func (h *ABFVppHandler) dumpABFPolicy() ([]*vppcalls.ABFDetails, error) {
+	var abfs []*vppcalls.ABFDetails
+	req := &abf.AbfPolicyDump{}
+	reqCtx := h.dumpChannel.SendMultiRequest(req)
+
+	for {
+		reply := &abf.AbfPolicyDetails{}
+		last, err := reqCtx.ReceiveReply(reply)
+		if err != nil {
+			return nil, err
+		}
+		if last {
+			break
+		}
+
+		// ACL name
+		aclName, _, exists := h.aclIndexes.LookupByIndex(reply.Policy.ACLIndex)
+		if !exists {
+			aclName = unknownName
+		}
+
+		// paths
+		var fwdPaths []*vpp_abf.ABF_ForwardingPath
+		for _, path := range reply.Policy.Paths {
+			// ip address
+			var nextHopIP net.IP = path.NextHop
+
+			// interface name
+			ifName, _, exists := h.ifIndexes.LookupBySwIfIndex(path.SwIfIndex)
+			if !exists {
+				ifName = unknownName
+			}
+
+			// base fields
+			fwdPath := &vpp_abf.ABF_ForwardingPath{
+				NextHopIp:       nextHopIP.String(),
+				InterfaceName:   ifName,
+				Vrf:             path.TableID,
+				Weight:          uint32(path.Weight),
+				Preference:      uint32(path.Preference),
+				Afi:             uint32(path.Afi),
+				NextHopId:       path.NextHopID,
+				RpfId:           path.RpfID,
+				ViaLabel:        path.ViaLabel,
+				Local:           uintToBool(path.IsLocal),
+				Drop:            uintToBool(path.IsDrop),
+				UdpEncap:        uintToBool(path.IsUDPEncap),
+				Unreachable:     uintToBool(path.IsUnreach),
+				Prohibit:        uintToBool(path.IsProhibit),
+				ResolveHost:     uintToBool(path.IsResolveHost),
+				ResolveAttached: uintToBool(path.IsResolveAttached),
+				Dvr:             uintToBool(path.IsDvr),
+				SourceLookup:    uintToBool(path.IsSourceLookup),
+			}
+
+			// label stack
+			var labelStack []*vpp_abf.ABF_ForwardingPath_Label
+			for _, label := range path.LabelStack {
+				labelEntry := &vpp_abf.ABF_ForwardingPath_Label{
+					IsUniform: uintToBool(label.IsUniform),
+					Label:     label.Label,
+					TTL:       uint32(label.TTL),
+					Exp:       uint32(label.Exp),
+				}
+
+				labelStack = append(labelStack, labelEntry)
+			}
+
+			fwdPaths = append(fwdPaths, fwdPath)
+		}
+
+		abfData := &vppcalls.ABFDetails{
+			ABF: &vpp_abf.ABF{
+				Index:           strconv.Itoa(int(reply.Policy.PolicyID)),
+				AclName:         aclName,
+				ForwardingPaths: fwdPaths,
+			},
+			Meta: &vppcalls.ABFMeta{
+				PolicyID: reply.Policy.PolicyID,
+			},
+		}
+
+		abfs = append(abfs, abfData)
+	}
+
+	return abfs, nil
+}
+
+func uintToBool(value uint8) bool {
+	if value == 0 {
+		return false
+	}
+	return true
+}
