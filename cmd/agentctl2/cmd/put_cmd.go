87c31cf8cdb4eae9b4921222967c6cb585fbd19e
Andrej Kozemcak <andrej.kozemcak@pantheon.tech>
2019-04-12 16:00:40 +0200

    Agentctl2: Add example command to CLI.
    Signed-off-by: Andrej Kozemcak <andrej.kozemcak@pantheon.tech>

--- cmd/agentctl2/cmd/put_cmd.go
+++ cmd/agentctl2/cmd/put_cmd.go
@@ -16,8 +16,17 @@ var putConfig = &cobra.Command{
 	Long: `
 	Put configuration file
 `,
-	Args: cobra.MinimumNArgs(2),
-	Run:  putFunction,
+	Args: cobra.RangeArgs(2, 2),
+	Example: ` Set route configuration for "vpp1":
+   $./agentctl2 -e 172.17.0.3:2379 put /vnf-agent/vpp1/config/vpp/v2/route/vrf/1/dst/10.1.1.3/32/gw/192.168.1.13 '{
+   "type": 1,
+   "vrf_id": 1,
+   "dst_network": "10.1.1.3/32",
+   "next_hop_addr": "192.168.1.13"
+    }'
+`,
+
+	Run: putFunction,
 }
 
 func init() {
