5eee90ce75f0723f0462810445ce540182340cf4
Ondrej Fabry <ofabry@cisco.com>
2019-07-03 09:39:52 +0200

    Update perf test
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- tests/perf/run_all.sh
+++ tests/perf/run_all.sh
@@ -11,17 +11,8 @@ function run() {
 	typ="${2-basic}"
 	requests="${3-${REQUESTS-1000}}"
 
-	export REPORT_DIR="${reports}/${test}_${requests}_${typ}"
-	./perf_test.sh "$test" "$requests"
-
 	export DEBUG_ENABLED=y
-
-	export REPORT_DIR="${reports}/${test}_${requests}_${typ}/cpu"
-	export DEBUG_PROFILE_MODE=cpu
-	./perf_test.sh "$test" "$requests"
-
-	export REPORT_DIR="${reports}/${test}_${requests}_${typ}/mem"
-	export DEBUG_PROFILE_MODE=mem
+	export REPORT_DIR="${reports}/${test}_${requests}_${typ}"
 	./perf_test.sh "$test" "$requests"
 }
 
