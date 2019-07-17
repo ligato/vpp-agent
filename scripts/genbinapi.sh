9313c984c138a9930d4c3eea980d8e20b7bd77fa
Ondrej Fabry <ofabry@cisco.com>
2019-07-17 06:47:06 +0200

    Update VPP to 19.08-rc0.666-g9082b43dd (#1413)
    * Update VPP to 19.08-rc0.666-g9082b43dd
    
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>
    
    * Update VPP to 19.08-rc0.673-ga2e4451db
    
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- scripts/genbinapi.sh
+++ scripts/genbinapi.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 go generate -x ./${VPP_BINAPI}
 
 find ${VPP_BINAPI} -maxdepth 2 -type f -name '*.patch' -exec \
-	patch --verbose --no-backup-if-mismatch -p1 -i {} \;
+	patch --no-backup-if-mismatch -p1 -i {} \;
