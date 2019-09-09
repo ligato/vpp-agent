6427c7796e87f89f5c1b10152c5b5c15c67f7db4
Vladimir Lavor <vlavor@cisco.com>
2019-09-09 14:08:54 +0200

    Add support for Punt plugin
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- plugins/vpp/puntplugin/puntplugin.go
+++ plugins/vpp/puntplugin/puntplugin.go
@@ -39,6 +39,7 @@ import (
 	_ "github.com/ligato/vpp-agent/plugins/vpp/puntplugin/vppcalls/vpp1901"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/puntplugin/vppcalls/vpp1904"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/puntplugin/vppcalls/vpp1908"
+	_ "github.com/ligato/vpp-agent/plugins/vpp/puntplugin/vppcalls/vpp2001"
 )
 
 // PuntPlugin configures VPP punt to host or unix domain socket entries and IP redirect entries using GoVPP.
