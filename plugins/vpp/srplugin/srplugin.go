de9b99c880ec9402441043208bf729df44b0e292
Vladimir Lavor <vlavor@cisco.com>
2019-09-09 14:12:56 +0200

    Add support for Segment routing plugin
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- plugins/vpp/srplugin/srplugin.go
+++ plugins/vpp/srplugin/srplugin.go
@@ -32,6 +32,7 @@ import (
 	_ "github.com/ligato/vpp-agent/plugins/vpp/srplugin/vppcalls/vpp1901"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/srplugin/vppcalls/vpp1904"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/srplugin/vppcalls/vpp1908"
+	_ "github.com/ligato/vpp-agent/plugins/vpp/srplugin/vppcalls/vpp2001"
 )
 
 // SRPlugin configures segment routing.
