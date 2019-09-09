ff93a144c7e57c72730804261e396d2287001ccb
Vladimir Lavor <vlavor@cisco.com>
2019-09-09 13:57:00 +0200

    Add support for IPSec plugin
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- plugins/vpp/ipsecplugin/ipsecplugin.go
+++ plugins/vpp/ipsecplugin/ipsecplugin.go
@@ -35,6 +35,7 @@ import (
 	_ "github.com/ligato/vpp-agent/plugins/vpp/ipsecplugin/vppcalls/vpp1901"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/ipsecplugin/vppcalls/vpp1904"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/ipsecplugin/vppcalls/vpp1908"
+	_ "github.com/ligato/vpp-agent/plugins/vpp/ipsecplugin/vppcalls/vpp2001"
 )
 
 // IPSecPlugin configures VPP security policy databases and security associations using GoVPP.
