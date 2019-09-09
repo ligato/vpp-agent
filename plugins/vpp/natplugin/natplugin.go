217232b40d4edb62715795af548b8acdbdcfb4ce
Vladimir Lavor <vlavor@cisco.com>
2019-09-09 14:07:43 +0200

    Add support for NAT plugin
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- plugins/vpp/natplugin/natplugin.go
+++ plugins/vpp/natplugin/natplugin.go
@@ -35,6 +35,7 @@ import (
 	_ "github.com/ligato/vpp-agent/plugins/vpp/natplugin/vppcalls/vpp1901"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/natplugin/vppcalls/vpp1904"
 	_ "github.com/ligato/vpp-agent/plugins/vpp/natplugin/vppcalls/vpp1908"
+	_ "github.com/ligato/vpp-agent/plugins/vpp/natplugin/vppcalls/vpp2001"
 )
 
 // NATPlugin configures VPP NAT.
