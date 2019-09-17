50c221d62a1a82bd27646903780b8c1e620e2f36
Vladimir Lavor <vlavor@cisco.com>
2019-09-17 14:12:32 +0200

    Fix ACL ICMP rules
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- plugins/vpp/aclplugin/vppcalls/vpp2001/acl_vppcalls.go
+++ plugins/vpp/aclplugin/vppcalls/vpp2001/acl_vppcalls.go
@@ -312,7 +312,7 @@ func icmpACL(icmpRule *acl.ACL_Rule_IpRule_Icmp, aclRule *vpp_acl.ACLRule) *vpp_
 		aclRule.SrcportOrIcmptypeLast = uint16(icmpRule.IcmpTypeRange.Last)
 		// ICMPv6 code range
 		aclRule.DstportOrIcmpcodeFirst = uint16(icmpRule.IcmpCodeRange.First)
-		aclRule.DstportOrIcmpcodeLast = uint16(icmpRule.IcmpCodeRange.First)
+		aclRule.DstportOrIcmpcodeLast = uint16(icmpRule.IcmpCodeRange.Last)
 	} else {
 		aclRule.Proto = vppcalls.ICMPv4Proto // IANA ICMPv4
 		aclRule.IsIPv6 = 0
