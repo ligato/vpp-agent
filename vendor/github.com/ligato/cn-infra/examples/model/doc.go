775b3ecfab801386dd489288bbeb5be22f7a8bdc
Jon Loeliger <jdl@netgate.com>
2019-10-16 10:54:53 -0500

    Remove the entire vendor/ subdirectory.
    With the use of "go mod", the vendor/ tree is not needed.
    
    Signed-off-by: Jon Loeliger <jdl@netgate.com>

--- vendor/github.com/ligato/cn-infra/examples/model/doc.go
+++
@@ -1,5 +0,0 @@
-// Package etcdexample explains how to generate Golang structures from
-// protobuf-formatted data.
-package etcdexample
-
-//go:generate protoc --proto_path=. --go_out=. example.proto
