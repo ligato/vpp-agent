630cdfdf34b4b52dd14d9088427cefb5be3bd1dc
Ondrej Fabry <ofabry@cisco.com>
2020-07-31 10:01:05 +0200

    Update proto tools to v2
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- scripts/protoc_gen_go.sh
+++ scripts/protoc_gen_go.sh
@@ -31,5 +31,6 @@ for dir in $protodirs; do
 	echo "$protofiles"
 
   	protoc --proto_path="${PROTO_PATH}" \
-  		--go_out="${PROTOC_GEN_GO_ARGS}" $protofiles
+  		--go_out="${PROTOC_GEN_GO_ARGS}" $protofiles \
+  		--go-grpc_out="${PROTOC_GEN_GO_ARGS}" $protofiles
 done
