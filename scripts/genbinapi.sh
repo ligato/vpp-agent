2e7251160b745764d58dc1a3a9798636c8ef70e6
Ondrej Fabry <ofabry@cisco.com>
2020-09-10 08:02:23 +0200

    fix: Use binapi-generator v0.3.5 for VPP binapi <2009
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- scripts/genbinapi.sh
+++ scripts/genbinapi.sh
@@ -4,6 +4,18 @@ set -euo pipefail
 export VPP_API_DIR=${VPP_API_DIR:-/usr/share/vpp/api}
 export VPP_VERSION=
 
+binapidir=$(basename "$VPP_BINAPI")
+if [ "$binapidir" == "vpp2005" ] || \
+   [ "$binapidir" == "vpp2001" ] || \
+   [ "$binapidir" == "vpp1908" ]
+then
+    (
+     cd $(mktemp -d)
+     echo "module x" > go.mod
+     GO111MODULE=on go get -v git.fd.io/govpp.git/cmd/binapi-generator@v0.3.5
+    )
+fi
+
 # Generate binapi
 go generate -x ./"${VPP_BINAPI}"
 
