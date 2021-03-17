c77dd8ba371d7b4f54754f7da244c5d5f5047db6
Milan Lenco <milan.lenco@pantheon.tech>
2021-03-17 11:16:22 +0100

    fix: bugs VPP-1968 and VPP-1970 were fixed in VPP 21.01
    Signed-off-by: Milan Lenco <milan.lenco@pantheon.tech>

--- tests/e2e/020_netalloc_test.go
+++ tests/e2e/020_netalloc_test.go
@@ -567,10 +567,8 @@ func TestVPPRoutesWithNetalloc(t *testing.T) {
 	checkItemsAreConfigured(true, true)
 
 	// check connection with ping
-	if ctx.VppRelease() < "21.01" { // ping is broken in VPP>=21.01 (https://jira.fd.io/browse/VPP-1970)
-		Expect(ctx.PingFromVPP(linuxLoopNet1IP)).To(Succeed())
-		Expect(ctx.PingFromVPP(linuxLoopNet2IP)).To(Succeed())
-	}
+	Expect(ctx.PingFromVPP(linuxLoopNet1IP)).To(Succeed())
+	Expect(ctx.PingFromVPP(linuxLoopNet2IP)).To(Succeed())
 	Expect(ctx.AgentInSync()).To(BeTrue())
 
 	// restart microservice
@@ -579,12 +577,10 @@ func TestVPPRoutesWithNetalloc(t *testing.T) {
 	checkItemsAreConfigured(true, true)
 
 	// check connection with ping (few packets will get lost before tables are refreshed)
-	if ctx.VppRelease() < "21.01" {
-		ctx.PingFromVPP(linuxLoopNet1IP)
-		ctx.PingFromVPP(linuxLoopNet2IP)
-		Expect(ctx.PingFromVPP(linuxLoopNet1IP)).To(Succeed())
-		Expect(ctx.PingFromVPP(linuxLoopNet2IP)).To(Succeed())
-	}
+	ctx.PingFromVPP(linuxLoopNet1IP)
+	ctx.PingFromVPP(linuxLoopNet2IP)
+	Expect(ctx.PingFromVPP(linuxLoopNet1IP)).To(Succeed())
+	Expect(ctx.PingFromVPP(linuxLoopNet2IP)).To(Succeed())
 	Expect(ctx.AgentInSync()).To(BeTrue())
 
 	// change IP addresses - the network items should be re-created
@@ -600,11 +596,9 @@ func TestVPPRoutesWithNetalloc(t *testing.T) {
 	checkItemsAreConfigured(false, true)
 
 	// check connection with ping
-	if ctx.VppRelease() < "21.01" {
-		Expect(ctx.PingFromVPP(linuxLoopNet1IP)).NotTo(Succeed())
-		Expect(ctx.PingFromVPP(linuxLoopNet1IP2)).To(Succeed())
-		Expect(ctx.PingFromVPP(linuxLoopNet2IP)).To(Succeed())
-	}
+	Expect(ctx.PingFromVPP(linuxLoopNet1IP)).NotTo(Succeed())
+	Expect(ctx.PingFromVPP(linuxLoopNet1IP2)).To(Succeed())
+	Expect(ctx.PingFromVPP(linuxLoopNet2IP)).To(Succeed())
 	Expect(ctx.AgentInSync()).To(BeTrue())
 
 	// de-allocate loopback IP in net2 - the connection to that IP should not work anymore
@@ -616,9 +610,7 @@ func TestVPPRoutesWithNetalloc(t *testing.T) {
 	checkItemsAreConfigured(false, false)
 
 	// can ping loop1, but cannot ping loop2
-	if ctx.VppRelease() < "21.01" {
-		Expect(ctx.PingFromVPP(linuxLoopNet1IP2)).To(Succeed())
-		Expect(ctx.PingFromVPP(linuxLoopNet2IP)).NotTo(Succeed())
-	}
+	Expect(ctx.PingFromVPP(linuxLoopNet1IP2)).To(Succeed())
+	Expect(ctx.PingFromVPP(linuxLoopNet2IP)).NotTo(Succeed())
 	Expect(ctx.AgentInSync()).To(BeTrue())
 }
