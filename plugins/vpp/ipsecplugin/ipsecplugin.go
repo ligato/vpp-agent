65a4e268d88a32c152ec158c20b7932aa1b8ec33
Ondrej Fabry <ondrej.fabry@pantheon.tech>
2022-06-29 08:20:04 +0200

    Drop support for VPP 20.05 & 20.09
    Signed-off-by: Ondrej Fabry <ondrej.fabry@pantheon.tech>

--- plugins/vpp/ipsecplugin/ipsecplugin.go
+++ plugins/vpp/ipsecplugin/ipsecplugin.go
@@ -34,8 +34,6 @@ import (
 	"go.ligato.io/vpp-agent/v3/plugins/vpp/ipsecplugin/descriptor/adapter"
 	"go.ligato.io/vpp-agent/v3/plugins/vpp/ipsecplugin/vppcalls"
 
-	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/ipsecplugin/vppcalls/vpp2005"
-	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/ipsecplugin/vppcalls/vpp2009"
 	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/ipsecplugin/vppcalls/vpp2101"
 	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/ipsecplugin/vppcalls/vpp2106"
 	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/ipsecplugin/vppcalls/vpp2202"
--- plugins/vpp/stnplugin/stnplugin.go
+++ plugins/vpp/stnplugin/stnplugin.go
@@ -27,8 +27,6 @@ import (
 	"go.ligato.io/vpp-agent/v3/plugins/vpp/stnplugin/descriptor/adapter"
 	"go.ligato.io/vpp-agent/v3/plugins/vpp/stnplugin/vppcalls"
 
-	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/stnplugin/vppcalls/vpp2005"
-	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/stnplugin/vppcalls/vpp2009"
 	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/stnplugin/vppcalls/vpp2101"
 	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/stnplugin/vppcalls/vpp2106"
 	_ "go.ligato.io/vpp-agent/v3/plugins/vpp/stnplugin/vppcalls/vpp2202"
