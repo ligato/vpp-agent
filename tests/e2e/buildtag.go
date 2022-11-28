e74b0d02c233e6a57ecff6e1bb00f4e5a8688a6d
Peter Motičák <peter.moticak@pantheon.tech>
2022-11-28 16:10:58 +0100

    Change buildtag name
    Signed-off-by: Peter Motičák <peter.moticak@pantheon.tech>

--- tests/e2e/buildtag.go
+++ tests/e2e/buildtag.go
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-//go:build e2etest
+//go:build e2e
 
 package e2e
 
--- tests/e2e/run_e2e.sh
+++ tests/e2e/run_e2e.sh
@@ -33,7 +33,7 @@ go build -o ./tests/e2e/agentctl.test \
 
 # Compile testing suite
 go test -c -o ./tests/e2e/e2e.test \
-	  -tags 'osusergo netgo e2etest' \
+	  -tags 'osusergo netgo e2e' \
     -ldflags '-w -s -extldflags "-static"' \
     -trimpath \
     ./tests/e2e/cases
