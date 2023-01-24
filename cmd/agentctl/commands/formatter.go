95306cc0e23787707f776e66ea4f50d352413945
Peter Motičák <peter.moticak@pantheon.tech>
2023-01-24 14:18:44 +0100

    Cleanup
    Signed-off-by: Peter Motičák <peter.moticak@pantheon.tech>

--- cmd/agentctl/commands/formatter.go
+++ cmd/agentctl/commands/formatter.go
@@ -140,7 +140,7 @@ func agoTmpl(t time.Time) time.Duration {
 
 func shortHumanDuration(d time.Duration) string {
 	if seconds := int(d.Seconds()); seconds < -1 {
-		return fmt.Sprintf("<invalid>")
+		return "<invalid>"
 	} else if seconds < 0 {
 		return "0s"
 	} else if seconds < 60 {
